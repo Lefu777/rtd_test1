ae5c840ee49c4a6e26b3ce949ce26c0221870947
Lefu777 <101187464+Lefu777@users.noreply.github.com>
2024-12-24 10:34:31 +0900

    - fix EPUB3 に必要らしいのでversion追加

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,9 @@ copyright = '2024, Lefu777'
 author = 'Lefu777'
 release = '1.0.0'
 
+# EPUB3 で必要らしい。
+version = "0.1.0"
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
